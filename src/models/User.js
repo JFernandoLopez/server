9949f99f42cb467191eb95119e690453c951d229
José Fernando López <fernandojlb@outlook.es>
2024-02-13 15:44:33 -0600

    building User model with id - name - email properties

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "User",
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV1,
+                allowNull: false,
+                primaryKey: true
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+        },
+        { timestamps: true, paranoid: true }
+    );
+};
