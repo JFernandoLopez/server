d7e33931cd227653b94cc3db22a7c9da4b39a703
José Fernando López <fernandojlb@outlook.es>
2024-02-13 15:34:52 -0600

    base building structure/three models/Room - Site - Users

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors')
 
 const app = express()
+app.use(express.json());
+app.use(cors())
+
+
 
 app.listen(3001, () => {
     console.log(`Server started on port: 3001`);
